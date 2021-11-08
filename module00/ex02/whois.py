dd972a723f5e5a884e3acf77d1b740994bd93923
twagner <twagner@student.42.fr>
2021-11-08 22:39:26 +0100

    ex02

---
+++ module00/ex02/whois.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) > 2:
+	print('ERROR')
+elif len(sys.argv) < 2:
+	print('ERROR')
+elif sys.argv[1].isdecimal == False:
+	print('ERROR')
+elif int(sys.argv[1]) == 0:
+	print("I'm  Zero.")
+elif int(sys.argv[1]) % 2 == 0:
+	print("I'm  Even.")
+elif int(sys.argv[1]) % 2 == 1:
+	print("I'm  Odd.")
