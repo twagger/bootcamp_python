f80cc7d4cbd12cead65d283b4df6299f747cf6a7
twagner <twagner@student.42.fr>
2022-04-19 16:19:09 +0200

    ex03 improved

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -1,26 +1,32 @@
 import sys
-import string 
+import string
+import textwrap
 
 def text_analyzer(text=''):
-	"""This function counts the number of upper characters, lower characters,
-	punctuation and spaces in a given text."""
+	"""
+	This function counts the number of upper characters, lower characters,
+	punctuation and spaces in a given text.
+	"""
 	if type(text) != str:
 		print("AssertionError : argument is not a string")
 	else:
 		while len(text) == 0:
 			text = raw_input("What is the text to analyze?\n")
-		print("The text contains " + str(len(text)) + " character(s):")
-		result = [0, 0, 0, 0]
-		for letter in text:
-			if letter.isupper():
-				result[0] += 1
-			if letter.islower():
-				result[1] += 1
-			if letter in string.punctuation:
-				result[2] += 1
-			if letter.isspace():
-				result[3] += 1
-		print("- " + str(result[0]) + " upper letter(s)")
-		print("- " + str(result[1]) + " lower letter(s)")
-		print("- " + str(result[2]) + " punctuation mark(s)")
-		print("- " + str(result[3]) + " space(s)")
+		print(textwrap.dedent("""\
+		The text contains {} character(s):
+		- {} upper letter(s)
+		- {} lower letter(s)
+		- {} punctiation mark(s)
+		- {} space(s)\
+		""".format(
+			len(text),
+			sum(1 for l in text if l.isupper()),
+			sum(1 for l in text if l.islower()),
+			sum(1 for l in text if l in string.punctuation),
+			sum(1 for l in text if l.isspace()),
+		)))
+
+if len(sys.argv) > 2:
+	print("Usage: python count.py [Argument]")
+else:
+	text_analyzer(sys.argv[1])
