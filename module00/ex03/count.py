fecbec638965eb22ead80a79eade87ed4ca7ebd1
twagner <twagner@student.42.fr>
2022-04-19 15:54:58 +0200

    working but probably not optimal

--- module00/ex03/count.py
+++ module00/ex03/count.py
@@ -1,19 +1,26 @@
 import sys
+import string 
 
-def text_analyser(str):
-	:
-
-# check arguments
-if len(sys.argv) <= 1:
-	print("Usage: python whois.py [Argument]")
-elif len(sys.argv) >= 3:
-	print("AssertionError: more than one argument are provided")
-else:
-	if sys.argv[1].isdigit() == False:
-		print("AssertionError: argument is not an integer")
-	elif int(sys.argv[1]) == 0:
-		print("I'm Zero.")
-	elif int(sys.argv[1]) % 2 == 0:
-		print("I'm Even.")
+def text_analyzer(text=''):
+	"""This function counts the number of upper characters, lower characters,
+	punctuation and spaces in a given text."""
+	if type(text) != str:
+		print("AssertionError : argument is not a string")
 	else:
-		print("I'm Odd.")
+		while len(text) == 0:
+			text = raw_input("What is the text to analyze?\n")
+		print("The text contains " + str(len(text)) + " character(s):")
+		result = [0, 0, 0, 0]
+		for letter in text:
+			if letter.isupper():
+				result[0] += 1
+			if letter.islower():
+				result[1] += 1
+			if letter in string.punctuation:
+				result[2] += 1
+			if letter.isspace():
+				result[3] += 1
+		print("- " + str(result[0]) + " upper letter(s)")
+		print("- " + str(result[1]) + " lower letter(s)")
+		print("- " + str(result[2]) + " punctuation mark(s)")
+		print("- " + str(result[3]) + " space(s)")
