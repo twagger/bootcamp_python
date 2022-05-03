a8c312959ee4f76364ead5f4fa745efcf76ca23b
twagner <twagner@student.42.fr>
2022-05-03 13:25:17 +0200

    almost ok ex02

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -5,7 +5,58 @@ print("\n\033[1;35m--{}: {}--\033[0m".format("Test 1", "Create a basic column ve
 v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
 print("Shape\t: {}\nValues\t: {}".format(v1.shape, v1.values))
 
-# Basic raw Vector test
-print("\n\033[1;35m--{}: {}--\033[0m".format("Test 2", "Create a basic raw vector"))
+# Basic row Vector test
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 2", "Create a basic row vector"))
 v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
 print("Shape\t: {}\nValues\t: {}".format(v1.shape, v1.values))
+
+# Transpose test Row to Column
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 3", "Transpose a Row vector to a Column"))
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = v1.T()
+print("Transposed vector shape is : {}".format(v2.shape))
+print("Shape\t: {}\nValues\t: {}".format(v2.shape, v2.values))
+
+# Transpose test Column to Row
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 4", "Transpose a Column vector to a Row"))
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+v2 = v1.T()
+print("Transposed vector shape is : {}".format(v2.shape))
+print("Shape\t: {}\nValues\t: {}".format(v2.shape, v2.values))
+
+# Dot product fail
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 5", "Dot product fail"))
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = Vector([[2.0, 4.0, 8.0, 16.0, 32.0]])
+try:
+    print("Dot product is : {}".format(v1.dot(v2)))
+except Exception as e:
+    print(e)
+
+# Dot product of two column vectors
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 6", "Dot product of two column vectors"))
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+v2 = Vector([[2.0], [4.0], [8.0], [16.0]])
+print("Dot product is : {}".format(v1.dot(v2)))
+
+# Dot product of two row vectors
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 7", "Dot product of two column vectors"))
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = Vector([[2.0, 4.0, 8.0, 16.0]])
+print("Dot product is : {}".format(v1.dot(v2)))
+
+# Addition two row vectors
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 8", "Addition of two row vectors"))
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = Vector([[2.0, 4.0, 8.0, 16.0]])
+print("V1\t: {}".format(v1.values))
+print("V2\t: {}".format(v2.values))
+print("Result of addition is : {}".format((v1 + v2).values))
+
+# Addition two column vectors
+print("\n\033[1;35m--{}: {}--\033[0m".format("Test 9", "Addition of two column vectors"))
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+v2 = Vector([[2.0], [4.0], [8.0], [16.0]])
+print("V1\t: {}".format(v1.values))
+print("V2\t: {}".format(v2.values))
+print("Result of addition is : {}".format((v1 + v2).values))
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -1,11 +1,101 @@
+from locale import setlocale
+
+
 class Vector:
 
+    # Constructor
     def __init__(self, values):
         if type(values) is not list:
-            raise TypeError
+            raise TypeError("Wrong type : {}".format(type(values)))
         else:
             self.values = values
             if len(values) == 1:
                 self.shape = (1, len(values[0]))
             else:
                 self.shape = (len(values), 1)
+    
+    # Member functions    
+    def dot(self, v2):
+        if v2.shape[0] == self.shape[0] and v2.shape[1] == self.shape[1]:
+            if self.shape[1] == 1:
+                return sum(self.values[i][0] * v2.values[i][0] for i in range(len(self.values))) 
+            else:
+                return sum(self.values[0][i] * v2.values[0][i] for i in range(len(self.values[0]))) 
+        else:
+            raise Exception("Vectors don't have the same shape")
+
+    def T(self):
+        if self.shape[0] == 1:
+            return Vector([[value] for value in self.values[0]])
+        else:
+            return Vector([[value[0] for value in self.values]])
+
+    # Operators overload
+    def __add__(self, rhs):
+        if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
+            if self.shape[1] == 1:
+                return Vector([[self.values[i][0] + rhs.values[i][0]] for i in range(len(self.values))]) 
+            else:
+                return Vector([[self.values[0][i] + rhs.values[0][i] for i in range(len(self.values[0]))]]) 
+        else:
+            raise Exception("Vectors don't have the same shape")
+
+    def __radd__(self, rhs):
+        if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
+            if self.shape[1] == 1:
+                return Vector([[rhs.values[i][0] + self.values[i][0]] for i in range(len(self.values))]) 
+            else:
+                return Vector([[rhs.values[0][i] + self.values[0][i] for i in range(len(self.values[0]))]]) 
+        else:
+            raise Exception("Vectors don't have the same shape")
+
+    def __sub__(self, rhs):
+        if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
+            if self.shape[1] == 1:
+                return Vector([[self.values[i][0] - rhs.values[i][0]] for i in range(len(self.values))]) 
+            else:
+                return Vector([[self.values[0][i] - rhs.values[0][i] for i in range(len(self.values[0]))]]) 
+        else:
+            raise Exception("Vectors don't have the same shape")
+
+    def __rsub__(self, rhs):
+        if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
+            if self.shape[1] == 1:
+                return Vector([[rhs.values[i][0] - self.values[i][0]] for i in range(len(self.values))]) 
+            else:
+                return Vector([[rhs.values[0][i] - self.values[0][i] for i in range(len(self.values[0]))]]) 
+        else:
+            raise Exception("Vectors don't have the same shape")
+
+    def __truediv__(self, scalar):
+        if scalar == 0:
+            raise Exception("Division by 0.")
+        else:
+            if self.shape[1] == 1:
+                return Vector([value / scalar for value in self.values]) 
+            else:
+                return Vector([[value / scalar] for value in self.values[0]]) 
+
+    def __rtruediv__(self, scalar):
+        raise Exception("Division of a scalar by a Vector is not defined here.")
+    
+    def __mul__(self, scalar):
+        if self.shape[1] == 1:
+            return Vector([value * scalar for value in self.values]) 
+        else:
+            return Vector([[value * scalar] for value in self.values[0]])
+    
+    def __rmul__(self, scalar):
+        if self.shape[1] == 1:
+            return Vector([scalar * value for value in self.values]) 
+        else:
+            return Vector([[scalar * value] for value in self.values[0]]) 
+
+    def __rsub__(self, rhs):
+        if rhs.shape[0] == self.shape[0] and rhs.shape[1] == self.shape[1]:
+            if self.shape[1] == 1:
+                return Vector([[rhs.values[i][0] - self.values[i][0]] for i in range(len(self.values))]) 
+            else:
+                return Vector([[rhs.values[0][i] - self.values[0][i] for i in range(len(self.values[0]))]]) 
+        else:
+            raise Exception("Vectors don't have the same shape")
