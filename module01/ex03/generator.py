b84a69169002e23b8ebef7dccb351b1e4d76120b
twagner <twagner@student.42.fr>
2022-05-03 17:26:36 +0200

    ex03 ok

---
+++ module01/ex03/generator.py
@@ -0,0 +1,26 @@
+from random import randrange
+
+def generator(text, sep=" ", option=None):
+    '''Splits the text according to sep value and yield the substrings.
+       option precise if a action is performed to the substrings before it is yielded.
+    '''
+    if type(text) is not str:
+        yield ("ERROR")
+    elif option is None:
+        for word in text.split(sep):
+            yield word
+    elif option is "shuffle":
+        unshuffled = text.split(sep)
+        shuffled = []
+        for word in unshuffled:
+            shuffled.insert(randrange(len(unshuffled)), word)
+        for word in shuffled:
+            yield word
+    elif option is "ordered":
+        ordered = text.split(sep)
+        ordered.sort()
+        for word in ordered:
+            yield word
+    elif option is "unique":
+        for word in set(text.split(sep)):
+            yield word
