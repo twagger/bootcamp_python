0c81e68366b7e147b1a9b13a7f2a726a63dac586
twagner <twagner@student.42.fr>
2022-05-10 09:33:39 +0200

    added global try except to handle type error

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -4,22 +4,25 @@ import textwrap
 
 # Progress bar on a list
 def ft_progress(lst):
-    i = 1
-    progress_bar_size = 30
-    start = time.time()
-    lstlen = len(lst)
-    for item in lst:
-        percent = i * 100 / lstlen
-        elapsed_time = time.time() - start
-        curr_size = progress_bar_size * percent // 100
-        progress_bar = ('=' * (int(curr_size) - 1)) + '>'
-        eta = elapsed_time / (percent / 100)
-        print(textwrap.dedent("""\
-        ETA : {:5.2f}s [{:3.0f}%][{:30s}] {}/{} | elapsed time {:.2f}s\
-        """.format(eta, percent, progress_bar, i, lstlen, elapsed_time)),
-                   end='\r')
-        i += 1
-        yield item
+    try:
+        i = 1
+        progress_bar_size = 30
+        start = time.time()
+        lstlen = len(lst)
+        for item in lst:
+            percent = i * 100 / lstlen
+            elapsed_time = time.time() - start
+            curr_size = progress_bar_size * percent // 100
+            progress_bar = ('=' * (int(curr_size) - 1)) + '>'
+            eta = elapsed_time / (percent / 100)
+            print(textwrap.dedent("""\
+            ETA : {:5.2f}s [{:3.0f}%][{:30s}] {}/{} | elapsed time {:.2f}s\
+            """.format(eta, percent, progress_bar, i, lstlen, elapsed_time)),
+                    end='\r')
+            i += 1
+            yield item
+    except:
+        raise TypeError("This is not a list")
 
 
 # Test program
