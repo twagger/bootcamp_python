beeb7a1510ad4cbce2d784f70378fbc142b1e714
twagner <twagner@student.42.fr>
2022-05-10 10:35:53 +0200

    adding one more test

--- module01/ex04/test.py
+++ module01/ex04/test.py
@@ -5,3 +5,8 @@ words = ["Le", "Lorem", "Ipsum", "est", "simple"]
 coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
 print("Zip evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
 print("Enumerate evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
+
+words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
+coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
+print("Zip evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
+print("Enumerate evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
