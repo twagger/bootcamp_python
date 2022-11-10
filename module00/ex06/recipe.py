bac5b795455cc5596895aa5ffa2c498ef2c6c7ef
twagner <twagner@student.42.fr>
2022-11-10 12:46:23 +0100

    fix: updated the code and added more control on input exception or wrong values

--- module00/ex06/recipe.py
+++ module00/ex06/recipe.py
@@ -1,4 +1,6 @@
-# DATA
+"""Cookbook"""
+
+# GLOBAL DATA
 sandwitch = {
     "ingredients": [
         "ham",
@@ -39,33 +41,43 @@ cookbook = {
 
 
 # FUNCTIONS
-def print_all_recipe_names(cookbook):
+def print_all_recipe_names():
+    """Print all receipe names"""
     print("\033[1;35m-- Cookbook recipes --\033[0m")
     for recipe_name in list(cookbook):
         print(recipe_name)
 
 
-def print_recipe_details(cookbook, recipe_name):
+def print_recipe_details(recipe_name):
+    """Print recipe details"""
     if recipe_name in cookbook:
         for key, value in cookbook[recipe_name].items():
-            print("\n\033[1;32m{}:\033[0m".format(key))
+            print(f'\n\033[1;32m{key}:\033[0m')
             if isinstance(value, list):
                 for item in value:
-                    print(" - {}".format(item))
+                    print(f' - {item}')
             else:
-                print(" - {}".format(value))
+                print(f' - {value}')
     else:
         print("\033[1;31m>> Error : unknown recipe\033[0m")
 
 
-def delete_recipe(cookbook, recipe_name):
+def delete_recipe(recipe_name):
+    """Delete recipe"""
     if recipe_name in cookbook:
         del cookbook[recipe_name]
     else:
         print("\033[1;31m>> Error : unknown recipe\033[0m")
 
+def prep_time_check(prep_time):
+    """Checks if the preparation time is in the proper format"""
+    if (prep_time.isdigit() is True
+        or ((prep_time[0] == '+') and prep_time[1:].isdigit() is True)):
+        return True
+    return False
 
-def add_recipe_from_input(cookbook):
+def add_recipe_from_input():
+    """Add recipe from input"""
     name = input("\033[1;35m>>> Enter a name:\n\033[0m")
     ingredients = []
     ingredient_item = input("\033[1;35m>>> Enter ingredients:\n\033[0m")
@@ -74,6 +86,8 @@ def add_recipe_from_input(cookbook):
         ingredient_item = input()
     meal = input("\033[1;35m>>> Enter a meal type:\n\033[0m")
     prep_time = input("\033[1;35m>>> Enter a preparation time:\n\033[0m")
+    while prep_time_check(prep_time) is False:
+        prep_time = input("\033[1;35m>>> Enter a proper preparation time:\n\033[0m")
     cookbook[name] = {
         "ingredients": ingredients,
         "meal": meal,
@@ -83,10 +97,11 @@ def add_recipe_from_input(cookbook):
 
 
 def print_menu():
+    """Print menu"""
     print("\033[1;32m********************************\033[0m")
     print("\033[1;35mWelcome to the Python Cookbook !")
     print("\033[1;32m********************************\033[0m")
-    print("List of available option:")
+    print("List of available options:")
     print("\t1: Add a recipe")
     print("\t2: Delete a recipe")
     print("\t3: Print recipe")
@@ -95,27 +110,35 @@ def print_menu():
 
 
 # PROGRAM
-choice = '0'
-while choice != '5':
-    print_menu()
-    choice = input("\033[1;35mPlease select an option:\n\033[0m>> ")
-    while choice not in ['1', '2', '3', '4', '5']:
-        choice = input(
-            "\033[1;35mPlease select an option (1 to 5):\n\033[0m>> ")
-    if choice == '1':
-        add_recipe_from_input(cookbook)
-    elif choice == '2':
-        recipe_name = input(
-            "\033[1;35mPlease enter the recipe name to delete:\n\033[0m>> ")
-        delete_recipe(cookbook, recipe_name)
-    elif choice == '3':
-        recipe_name = input("""\
-            \033[1;35mPlease enter a recipe name to get its details:
-            \033[0m>> """)
-        print_recipe_details(cookbook, recipe_name)
-    elif choice == '4':
-        print_all_recipe_names(cookbook)
-    elif choice == '5':
-        print("\033[1;32mBye 👋\033[0m")
-    if choice != '5':
-        pause = input("\n(Press <enter> to display menu)\n")
+def main():
+    """Main program"""
+    choice = '0'
+    while choice != '5':
+        try:
+            print_menu()
+            choice = input("\033[1;35mPlease select an option:\n\033[0m>> ")
+            while choice not in ['1', '2', '3', '4', '5']:
+                choice = input(
+                    "\033[1;35mPlease select an option (1 to 5):\n\033[0m>> ")
+            if choice == '1':
+                add_recipe_from_input()
+            elif choice == '2':
+                recipe_name = input(
+                    "\033[1;35mPlease enter the recipe name to delete:\n\033[0m>> ")
+                delete_recipe(recipe_name)
+            elif choice == '3':
+                recipe_name = input(
+                    "\033[1;35mPlease enter a recipe name to get its details:\n\033[0m>> ")
+                print_recipe_details(recipe_name)
+            elif choice == '4':
+                print_all_recipe_names()
+            elif choice == '5':
+                print("\033[1;32mBye 👋\033[0m")
+            if choice != '5':
+                input("\n(Press <enter> to display menu)\n")
+        except EOFError:
+            print('\n')
+            continue
+
+
+main()
