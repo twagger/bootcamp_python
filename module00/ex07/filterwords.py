b7c4c5cf0017d355ebe09d46ae42fdb48e63be47
twagner <twagner@student.42.fr>
2022-11-10 15:05:59 +0100

    fix: better parsing and usage of comprehension list in one function instead of nested conditions

--- module00/ex07/filterwords.py
+++ module00/ex07/filterwords.py
@@ -1,19 +1,34 @@
+"""This module filters words in a sentence"""
 import sys
 import string
 
 
 def clear_punctuation(word):
-    result = ""
-    for letter in word:
-        if letter not in string.punctuation:
-            result += letter
-    return result
+    """clear the punctuation from a string"""
+    return ''.join([letter for letter in word if letter
+                    not in string.punctuation])
 
 
-if len(sys.argv) != 3:
-    print("Usage: python filterwords.py [string] [int]")
-else:
-    words = sys.argv[1].split(' ')
-    words_no_punct = [clear_punctuation(word) for word in words]
-    result = [word for word in words_no_punct if len(word) > int(sys.argv[2])]
-    print(result)
+def integer_check(integer):
+    """Check if the string in param is an integer"""
+    if integer.isdigit() is True or ((integer[0] == '-' or integer[0] == '+')
+                                     and integer[1:].isdigit() is True):
+        return True
+    return False
+
+
+def main():
+    """Main program"""
+    if len(sys.argv) != 3:
+        print("Error: wrong number of arguments")
+    elif integer_check(sys.argv[2]) is False:
+        print("Error: Second parameter is not a proper integer")
+    else:
+        words = sys.argv[1].split(' ')
+        words_no_punct = [clear_punctuation(word) for word in words]
+        result = [word for word in words_no_punct if len(
+            word) > int(sys.argv[2])]
+        print(result)
+
+
+main()
