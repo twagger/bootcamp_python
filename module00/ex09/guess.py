6faa8abd1ce8b02a565818f8c79e361b593f4f5e
twagner <twagner@student.42.fr>
2022-11-11 09:08:42 +0100

    fix: improving controls on ex10 and fixing norm on ex08

--- module00/ex09/guess.py
+++ module00/ex09/guess.py
@@ -1,38 +1,45 @@
+"""This module guess a number between 1 and 99"""
 import random
 
-print("""\
-This is an interactive guessing game!
-You have to enter a number between 1 and 99 to find out the secret number.
-Type 'exit' to end the game.
-Good luck!
-""")
-guess = ''
-num = random.randint(1, 99)
-trials = 0
-while 1:
-    guess = input("What's your guess between 1 and 99?\n>> ")
-    if guess == 'exit':
-        print("Goodbye!")
-        break
-    trials += 1
-    try:
-        int(guess)
-    except ValueError:
-        print("That's not a number.")
-        continue
-    if int(guess) > num:
-        print("Too high!")
-    elif int(guess) < num:
-        print("Too low!")
-    else:
-        if num == 42:
-            print("""\
-            The answer to the ultimate question of life, \
-            the universe and everything is 42.""")
-        if trials == 1:
-            print("Congratulations! You got it on your first try!")
+
+def main():
+    """Main program"""
+    print("This is an interactive guessing game!\n"
+          "You have to enter a number between 1 and 99 to find out the secret "
+          "number.\nType 'exit' to end the game.\n"
+          "Good luck!\n")
+    guess = ''
+    num = random.randint(1, 99)
+    trials = 0
+    while 1:
+        try:
+            guess = input("What's your guess between 1 and 99?\n>> ")
+        except EOFError:
+            print('\n')
+            continue
+        if guess == 'exit':
+            print("Goodbye!")
             break
+        trials += 1
+        try:
+            int(guess)
+        except ValueError:
+            print("That's not a number.")
+            continue
+        if int(guess) > num:
+            print("Too high!")
+        elif int(guess) < num:
+            print("Too low!")
         else:
+            if num == 42:
+                print("The answer to the ultimate question of life, "
+                      "the universe and everything is 42.")
+            if trials == 1:
+                print("Congratulations! You got it on your first try!")
+                break
             print("Congratulations, you've got it !")
-            print("You won in {} attempts!".format(trials))
+            print(f'You won in {trials} attempts!')
             break
+
+
+main()
