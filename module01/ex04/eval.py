abd20f0ae8dd88080f56798dfe27199634f3f4ec
twagner <twagner@student.42.fr>
2022-11-14 16:15:58 +0100

    fix: small fixes

--- module01/ex04/eval.py
+++ module01/ex04/eval.py
@@ -1,26 +1,27 @@
-from unittest import result
-
+"""Evaluator module"""
 
 class Evaluator:
+    """Evaluator class"""
+    def __init__(self):
+        pass
 
-    def zip_evaluate(coefs, words):
+    def zip_evaluate(self, coefs, words):
         """zip_evaluate creates a tuple with two lists and \
         compute the sum of the lengths of every words of a \
         given list weighted by a list of coefficinents coefs"""
-        if (len(coefs) == len(words)):
+        if len(coefs) == len(words):
             zip_list = list(zip(coefs, words))
             result = sum(zip_list[i][0] * len(zip_list[i][1])
                          for i in range(len(zip_list)))
             return result
-        else:
-            return -1
+        return -1
 
     def enumerate_evaluate(self, coefs, words):
         """enumerate_evaluate creates a counter that can be used \
         as an index to access list entries"""
-        if (len(coefs) == len(words)):
+        if len(coefs) == len(words):
+            result = 0
             for index, value in enumerate(words):
-                result += sum(coefs[index] * len(value))
+                result += coefs[index] * len(value)
             return result
-        else:
-            return -1
+        return -1
--- module01/ex04/test.py
+++ module01/ex04/test.py
@@ -1,12 +1,19 @@
-
+"""Test program"""
 from eval import Evaluator
 
-words = ["Le", "Lorem", "Ipsum", "est", "simple"]
-coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
-print("Zip evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
-print("Enumerate evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
 
-words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
-coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
-print("Zip evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
-print("Enumerate evaluate : {}".format(Evaluator.zip_evaluate(coefs, words)))
+def main():
+    """Test program"""
+
+    words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+    coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+    print(f'Zip evaluate : {Evaluator().zip_evaluate(coefs, words)}')
+    print(f'Enumerate evaluate : {Evaluator().enumerate_evaluate(coefs, words)}')
+
+    words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
+    coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
+    print(f'Zip evaluate : {Evaluator().zip_evaluate(coefs, words)}')
+    print(f'Enumerate evaluate : {Evaluator().enumerate_evaluate(coefs, words)}')
+
+
+main()
