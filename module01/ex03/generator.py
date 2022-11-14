e2542f0ceca5b3c526b6c029f284a9f9b9f320bd
twagner <twagner@student.42.fr>
2022-11-14 15:10:52 +0100

    fix: fixing orders for the 2 last tests

--- module01/ex03/generator.py
+++ module01/ex03/generator.py
@@ -1,3 +1,4 @@
+"""Generator module"""
 from random import randrange
 
 
@@ -6,23 +7,27 @@ def generator(text, sep=" ", option=None):
        option precise if a action is performed to the substrings before \
        it is yielded.
     '''
-    if type(text) is not str:
-        yield ("ERROR")
+    if not isinstance(text, str):
+        yield "ERROR"
     elif option is None:
         for word in text.split(sep):
             yield word
-    elif option is "shuffle":
+    elif option == "shuffle":
         unshuffled = text.split(sep)
         shuffled = []
         for word in unshuffled:
             shuffled.insert(randrange(len(unshuffled)), word)
         for word in shuffled:
             yield word
-    elif option is "ordered":
+    elif option == "ordered":
+        # Sort by alphabetical order and not ASCII order (default)
         ordered = text.split(sep)
-        ordered.sort()
+        ordered = sorted(ordered, key=str.lower, reverse=False)
         for word in ordered:
             yield word
-    elif option is "unique":
-        for word in set(text.split(sep)):
+    elif option == "unique":
+        # Careful not to change the order
+        non_unique = text.split(sep)
+        unique = sorted(set(non_unique), key=non_unique.index)
+        for word in unique:
             yield word
--- module01/ex03/test.py
+++ module01/ex03/test.py
@@ -1,31 +1,33 @@
+"""Test module"""
 from generator import generator
 
-print("\n\033[1;35m--{}: {}--\033[0m"
-      .format("Test 0", "Text is not string"))
-text = 42
-for word in generator(text, sep=" ", option="shuffle"):
-    print(word)
 
-text = "Le Lorem Ipsum est simplement du faux texte."
+def main():
+    """Main program"""
+    print("\n\033[1;35m--Test 0: Text is not string--\033[0m")
+    text = 42
+    for word in generator(text, sep=" ", option="shuffle"):
+        print(word)
 
-print("\n\033[1;35m--{}: {}--\033[0m"
-      .format("Test 1", "Text with sep ' ' and no option"))
-for word in generator(text, sep=" "):
-    print(word)
+    text = "Le Lorem Ipsum est simplement du faux texte."
 
-print("\n\033[1;35m--{}: {}--\033[0m"
-      .format("Test 2", "Text with sep ' ' and shuffle option"))
-for word in generator(text, sep=" ", option="shuffle"):
-    print(word)
+    print("\n\033[1;35m--Test 1: Text with sep ' ' and no option--\033[0m")
+    for word in generator(text, sep=" "):
+        print(word)
 
-print("\n\033[1;35m--{}: {}--\033[0m"
-      .format("Test 3", "Text with sep ' ' and ordered option"))
-for word in generator(text, sep=" ", option="ordered"):
-    print(word)
+    print("\n\033[1;35m--Test 2: Text with sep ' ' and shuffle option--\033[0m")
+    for word in generator(text, sep=" ", option="shuffle"):
+        print(word)
+    
+    print("\n\033[1;35m--Test 3: Text with sep ' ' and ordered option--\033[0m")
+    for word in generator(text, sep=" ", option="ordered"):
+        print(word)
 
-text = "Le Le Lorem Ipsum est simplement du du Ipsum faux texte."
+    text = "Le Le Lorem Ipsum est simplement du du Ipsum faux texte."
+# Pas dans le bon ordre
+    print("\n\033[1;35m--Test 4: Text with sep ' ' and unique option--\033[0m")
+    for word in generator(text, sep=" ", option="unique"):
+        print(word)
 
-print("\n\033[1;35m--{}: {}--\033[0m"
-      .format("Test 4", "Text with sep ' ' and unique option"))
-for word in generator(text, sep=" ", option="unique"):
-    print(word)
+
+main()
