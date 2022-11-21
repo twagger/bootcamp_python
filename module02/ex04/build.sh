f078bf5c6ca09467a8e56713b1aeac456373e09b
twagner <twagner@student.42.fr>
2022-11-21 11:18:34 +0100

    fix: working package build and import

--- module02/ex04/build.sh
+++ module02/ex04/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# Update build packages
+# Get / Update build packages
 python -m pip install --upgrade pip setuptools wheel twine
 
+# Build the package
+python3 -m build
+
+# Build the .egg format
+python setup.py bdist_egg
+
+# Install the package
+pip install ./dist/my_minipack-1.0.0-py3-none-any.whl
+
+# Clean the build folder
+rm -rf ./build
--- module02/ex04/setup.py
+++ module02/ex04/setup.py
@@ -1,4 +1,28 @@
 """Setup script"""
 
 
-from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name = 'my_minipack',
+    version = '1.0.0',
+    summary = 'How to create a package in python.',
+    home_page = None,
+    author = 'twagner',
+    author_email = 'twagner@student.42.fr',
+    license = 'GPLv3',
+    location = '~/goinfre/miniconda3/envs/test_env/lib/python3.7/site-packages',
+    metadata_version = '2.1',
+    installer = 'pip',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Students',
+        'Topic :: Education',
+        'Topic :: HowTo',
+        'Topic :: Package',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
