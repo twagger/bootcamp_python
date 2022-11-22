8582d4d9f12e073464f762c4a3334837897227cf
twagner <twagner@student.42.fr>
2022-11-22 10:18:26 +0100

    fix: completing tests and updating reduce exceptions

--- module02/ex00/ft_reduce.py
+++ module02/ex00/ft_reduce.py
@@ -13,9 +13,16 @@ def ft_reduce(function_to_apply, iterable):
     try:
         if not callable(function_to_apply):
             return None
+        #iterable check
+        try:
+            iter(iterable)
+        except TypeError as exc:
+            raise TypeError("reduce() arg 2 must support iteration")from exc
         result = iterable[0]
         for item in iterable[1:]:
             result = function_to_apply(result, item)
         return result
     except TypeError as exc:
         raise TypeError(exc) from exc
+    except IndexError as exc:
+        raise TypeError("reduce() of empty iterable with no initial value") from exc
