ed059d1e677981ec40619e56619fcea951d36c3e
twagner <twagner@student.42.fr>
2022-12-01 11:27:27 +0100

    progress: finishing the mandatory part. Now I just need to plot everything

--- module03/ex04/Kmeans.py
+++ module03/ex04/Kmeans.py
@@ -2,6 +2,7 @@
 import sys
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class KmeansClustering:
@@ -42,7 +43,8 @@ class KmeansClustering:
             for i in range(int(self.ncentroid))
         ]
         # sum the sse by centroid
-        sse_by_centroid = np.array([np.sum(sse_part) for sse_part in sse_by_row])
+        sse_by_centroid = np.array([np.sum(sse_part)
+                                   for sse_part in sse_by_row])
         # sum the sse on the entire set
         sse = np.sum(sse_by_centroid)
         return sse
@@ -51,7 +53,7 @@ class KmeansClustering:
         """
         perform K-means multiple time and return the best centroïd array
         according to the SSE values
-        """,
+        """
         # init a global sse array with all information from iterations
         sse = np.empty((0, int(self.ncentroid), 4), dtype=np.float32)
         for _ in range(iterations):
@@ -61,13 +63,11 @@ class KmeansClustering:
             clusters = self.predict(dataset)
             # calculate sse
             current_sse = self.__calculate_sse(dataset, clusters)
-            print(current_sse)
             # concatenate sse and centroids in a single array
             current_sse_centroid = np.hstack(
                 (np.full((self.centroids.shape[0], 1), current_sse),
                  self.centroids))
             current_sse_centroid = np.expand_dims(current_sse_centroid, axis=0)
-            print(current_sse_centroid)
 
             # add the current set to the global sse array
             sse = np.vstack((sse, current_sse_centroid))
@@ -76,7 +76,36 @@ class KmeansClustering:
         lowest_sse = sse[:, :, 0].min()
         # filter the global matrix to keep only the best set of centroids
         self.centroids = sse[sse[:, :, 0] == lowest_sse][:4, 1:]
-        print(self.centroids)
+
+    def get_region(self):
+        """
+        according to the caracteristics of the centroids, associate a region
+        with each centroid.
+        Then add a "label" column to the matrix in parameter and return it.
+        """
+        belt, venus, earth, mars = 0, 0, 0, 0
+        labels = [None] * 4
+
+        # 1. Get max and min for useful dimension
+        max_height = np.argmax(self.centroids[:, 0])
+        min_height = np.argmin(self.centroids[:, 0])
+        max_weight = np.argmax(self.centroids[:, 1])
+        min_weight = np.argmin(self.centroids[:, 1])
+        min_density = np.argmin(self.centroids[:, 2])
+
+        # 2. Find regions
+        belt = max_height if max_height == min_density == max_weight else max_weight
+        earth = min_height
+        venus = min_weight
+        mars = 6 - belt - earth - venus
+
+        # 3. Assign labels
+        labels[belt] = 'Belt'
+        labels[earth] = 'Earth'
+        labels[venus] = 'Venus'
+        labels[mars] = 'Mars'
+
+        return labels
 
     def fit(self, X):  # entraine sur les donnees
         """
@@ -166,19 +195,50 @@ def main(**kwargs):
         return None
 
     # 4. data to nd array
-    data = np.array(rows, dtype=float)[:, 1:]
+    dataset = np.array(rows, dtype=float)[:, 1:]
 
     # 5. create a Kmeans clustering object
     kmc = KmeansClustering(max_iter=max_iter, ncentroid=ncentroid)
 
     # 6. fit the Kmeans using the dataset
-    kmc.do_multiple_kmeans(data)
+    kmc.do_multiple_kmeans(dataset)
 
     # 7. predict using the dataset to get the cluster indexes
-    cluster_prediction = kmc.predict(data)
+    prediction = kmc.predict(dataset)
 
-    # 8. label the clusters
-    labels = ['Venus', 'Earth', 'Mars', 'Belt']
+    # 8. associate region if ncentroids == 4
+    labels = None
+    if int(ncentroid) == 4:
+        labels = kmc.get_region()
+
+    # 8. display detailed information
+    print(f'{"":-<4}{"":-<10}{"":-<10}{"":-<20}{"":-<20}{"":-<20}')
+    print(f'{"id":<4}{"region":<10}{"indiv.":<10}{"height":<20}{"weight":<20}{"density":<20}')
+    print(f'{"":-<4}{"":-<10}{"":-<10}{"":-<20}{"":-<20}{"":-<20}')
+
+    [print(f'{index:<4}{labels[index]:<10}{np.count_nonzero(prediction == index):<10}{height:<20}{weight:<20}{density:<20}')
+     for index, (height, weight, density) in enumerate(kmc.centroids)]
+    
+    print(f'{"":-<4}{"":-<10}{"":-<10}{"":-<20}{"":-<20}{"":-<20}')
+
+    # 9. 2D plot the clusters
+
+    # 10. 3D plot the clusters
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+
+    # for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
+    #     xs = randrange(n, 23, 32)
+    #     ys = randrange(n, 0, 100)
+    #     zs = randrange(n, zlow, zhigh)
+    #     ax.scatter(xs, ys, zs, marker=m)
+    # ax.set_xlabel('height')
+    # ax.set_ylabel('weight')
+    # ax.set_zlabel('bone density')
+    # plt.show()
+
+    # X. label the clusters
+    # labels = ['Venus', 'Earth', 'Mars', 'Belt']
 
 
 if __name__ == '__main__':
