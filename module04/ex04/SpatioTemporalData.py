341ae5c3b1bf1c64ad4106918b9db1f71e7b5b94
twagner <twagner@student.42.fr>
2022-12-06 16:18:44 +0100

    progress: ex04 ok

---
+++ module04/ex04/SpatioTemporalData.py
@@ -0,0 +1,52 @@
+"""Spatio temporal data"""
+import pandas as pd
+
+
+class SpatioTemporalData():
+    """SpatioTemporalData"""
+
+    def __init__(self, df: pd.DataFrame):
+        """Constructor"""
+        self.df = df
+
+    def when(self, location: str) -> str:
+        """
+        takes a location as an argument and returns a list containing the
+        years where games were held in the given location,
+        """
+        if isinstance(location, str):
+            return list(self.df[self.df['City'] == location].Year.unique())
+
+
+    def where(self, date: int) -> list:
+        """
+        takes a date as an argument and returns the location where the
+        Olympics took place in the given year
+        """
+        if isinstance(date, int):
+            return list(self.df[self.df['Year'] == date].City.unique())
+
+
+if __name__ == '__main__':
+
+    # Import file loader
+    from FileLoader import FileLoader
+
+    loader = FileLoader()
+    data = loader.load("../athlete_events.csv")
+
+    if data is None:
+        print('Error with the file')
+    else:
+        
+        sp = SpatioTemporalData(data)
+
+        print(sp.where(2000))
+        # output is: ['Sydney']
+
+        print(sp.where(1980))
+        # output is: ['Lake Placid', 'Moskva'] If a single of these locations
+        # is returned it's ok.
+
+        print(sp.when('London'))
+        # output is: [2012, 1948, 1908]
